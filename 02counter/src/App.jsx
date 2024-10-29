fa2df300571793751dc0eed72d7d21e9ddb72378
Sadik Ali <sadikofficial011@gmail.com>
2024-10-29 19:57:04 +0530

    do some zcss changes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -21,11 +19,13 @@ function App() {
 
   return (
     <>
-    <h1>First REACT Project</h1>
+    <h1>Counter App in React</h1>
     <h2>Counter value: {counter}</h2>
-    <button onClick={addValue} >Add Value {counter}</button>
+   <div className="btn">
+   <button onClick={addValue} >Add Value </button>
     
-    <button onClick={decValue} >Decease Value {counter}</button>
+    <button onClick={decValue} >Decease Value </button>
+   </div>
     </>
   )
 }
