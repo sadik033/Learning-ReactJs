f60895f615e8cd8aa700552f1389172d25020dd4
Sadik Ali <sadikofficial011@gmail.com>
2024-11-13 11:34:17 +0530

    fetch data with useEffect

--- Countries_Apiproject/src/component/CountriesList.jsx
+++ Countries_Apiproject/src/component/CountriesList.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
-import CountriesData from "../../CountriesData";
+import React, { useEffect, useState } from "react";
+// import CountriesData from "../../CountriesData";
 import CountryCard from "./CountryCard";
 
 
 export default function CountriesList({query}) {
+
+ 
+  const [CountriesData, setCountriesData] = useState([])
+  const [count, setCount]= useState(0)
+
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => res.json())
+      .then((data) =>{
+        setCountriesData(data)
+      })
+  },[])
+
+  useEffect(()=>{
+    console.log('Hiii')
+  },[count])
+
   return (
     <>
+    <h1>{count}</h1>
+    <button onClick={()=> setCount(count+1)} >Increse</button>
     <div className="countries-container">{
     CountriesData.filter((country) => 
     country.name.common.toLowerCase().includes(query) ).map( (country) => {
