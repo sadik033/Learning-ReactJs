22c3d965954f8debb62694b9ae052c81b2c2b9c2
Sadik Ali <sadikofficial011@gmail.com>
2025-02-10 09:38:55 +0530

    Dark Mode

--- Countries_Apiproject/src/component/Header.jsx
+++ Countries_Apiproject/src/component/Header.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react"
+
 export default function Header() {
+    const [isDark , setIsDark] = useState(JSON.parse(localStorage.getItem('isDarkMode')));
+
+    if(isDark){
+        document.body.classList.add('dark')
+    } else{
+        document.body.classList.remove('dark')
+    }
+
     return (
         <header className="header-container">
        <div className="header-content">
         <h2 className="title"><a href="/">Where in the World?</a></h2>
-        <p className="theme-changer"> <i className="fa-regular fa-moon"></i>&nbsp;&nbsp;Dark Mode</p>
+        <p className="theme-changer" onClick={ ()=>{
+            
+            setIsDark(!isDark)
+            localStorage.setItem('isDarkMode',!isDark)
+        } }>
+             <i className={`fa-solid fa-${isDark ? 'sun' : 'moon'}`}></i>
+             &nbsp;&nbsp; {isDark? 'Light' : 'Dark'} Mode</p>
        </div>
     </header>
     )
