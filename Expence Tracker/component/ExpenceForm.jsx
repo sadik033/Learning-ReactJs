15ef87cf8783e45303003fd577bc62fbbb6a1116
Sadik Ali <sadikofficial011@gmail.com>
2025-03-05 10:15:38 +0530

    Work With Forms in React

---
+++ Expence Tracker/component/ExpenceForm.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function ExpenceForm(){
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const formData = new FormData(e.target);
+    const data ={}
+    for (const [key ,value] of formData.entries()) {
+      data[key] = value
+    }
+    console.log(data);
+  }
+    return(
+        <form className="expense-form" onSubmit={handleSubmit} >
+        <div className="input-container">
+          <label htmlFor="title">Title</label>
+          <input id="title" name="title" />
+        </div>
+        <div className="input-container">
+          <label htmlFor="category">Category</label>
+          <select id="category" name="category" >
+                <option value="" hidden>Select Category</option>
+                <option value="grocery">Grocery</option>
+                <option value="clothes">Clothes</option>
+                <option value="bills">Bills</option>
+                <option value="education">Education</option>
+                <option value="medicine">Medicine</option>
+              </select>
+        </div>
+        <div className="input-container">
+          <label htmlFor="amount">Amount</label>
+          <input id="amount" name="amount" />
+        </div>
+        <button className="add-btn">Add</button>
+      </form>
+    )
+}
---
+++ Expence Tracker/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import ExpenceForm from '../component/ExpenceForm'
+import ExpenceTable from '../component/ExpenceTable'
+import expenceData from '../component/expenceData'
+
+function App() {
+  const [expenses, setExpenses] = useState(expenceData)
+
+  return (
+    <main>
+    <h1>Track Your Expense</h1>
+    <div className="expense-tracker">
+      <ExpenceForm/>
+      <ExpenceTable expenses={expenses} /> 
+    </div>
+  </main>
+  )
+}
+
+export default App
