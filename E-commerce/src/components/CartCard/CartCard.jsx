310f85e157cd00ce7b22594cc078734d0b709bde
Sadik Ali <sadikofficial011@gmail.com>
2025-05-19 01:10:49 +0530

    setup cart page

---
+++ E-commerce/src/components/CartCard/CartCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./CartCard.css"
+function CartCard(){
+    return(
+        <div className="cartcard">
+            <div className="left-card">
+                <img src="" alt="" />
+                <div className="name-price">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div className="right-card"></div>
+        </div>
+    )
+}
+
+export default CartCard
---
+++ E-commerce/src/pages/cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Cart.css"
+import CartCard from "../../components/CartCard/CartCard";
+function Cart(){
+    return(
+        <div className="cart">
+            <CartCard/>
+        </div>
+    )
+}
+
+export default Cart
