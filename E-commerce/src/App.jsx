24b17ea537bd717364d577b62276db0aa4afdb55
Sadik Ali <sadikofficial011@gmail.com>
2025-05-19 18:05:52 +0530

    style cart card

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Nav from "./components/nav/Nav";
 import Shop from "./pages/shop/Shop";
 import Footer from './components/Footer/Footer'
+import Cart from "./pages/cart/Cart";
 
 function App(){
   return(
@@ -14,6 +15,7 @@ function App(){
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/shop' element={<Shop/>}/>
+          <Route path='/cart' element={<Cart/>}/>
         </Routes>
 
         <Footer/>
--- E-commerce/src/components/CartCard/CartCard.jsx
+++ E-commerce/src/components/CartCard/CartCard.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./CartCard.css"
+import image1 from "../../assets/image1.jpg"
+import { MdDeleteSweep } from "react-icons/md";
 function CartCard(){
     return(
         <div className="cartcard">
             <div className="left-card">
-                <img src="" alt="" />
+                <img src={image1} alt="" />
                 <div className="name-price">
-                    <span></span>
-                    <span></span>
+                    <span>Samsung</span>
+                    <span>Rs 15000/-</span>
                 </div>
             </div>
-            <div className="right-card"></div>
+            <div className="right-card">
+                <button>Remove <MdDeleteSweep /></button>
+            </div>
         </div>
     )
 }
