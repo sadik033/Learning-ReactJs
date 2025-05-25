bd16472c8b19121d59647a9a5cbd74d69b7063be
Sadik Ali <sadikofficial011@gmail.com>
2025-05-26 00:42:37 +0530

    create sidebar

--- Admin-Dashboard/src/App.jsx
+++ Admin-Dashboard/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import React from 'react'
+import Sidebar from './component/Sidebar/Sidebar'
 
 function App() {
     return (
-      <div className='App'>
+      <div className="App">
         <div className="AppGlass">
-          
+          <Sidebar/>
+          <div></div>
+          <div></div>
         </div>
       </div>
     
--- Admin-Dashboard/src/component/Sidebar/Sidebar.jsx
+++ Admin-Dashboard/src/component/Sidebar/Sidebar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Sidebar.css";
+import logo from "../../images/logo.png";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="Sidebar">
+      <div className="logo">
+        <img src={logo} alt="" />
+        <span>
+          Sh<span>o</span>ps
+        </span>
+      </div>
+       {/* menu  */}
+      <div className="menu">
+        <div className="menuItem">
+          <div>
+            <i class="ri-home-3-line"></i>
+          </div>
+          <span>Dashboard</span>
+        </div>
 
-export default Sidebar
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
