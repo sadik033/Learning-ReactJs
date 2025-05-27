9658cad3405847234ba531500de63ff671ff3184
Sadik Ali <sadikofficial011@gmail.com>
2025-05-27 17:08:14 +0530

    highlight sidebar

--- Admin-Dashboard/src/component/Sidebar/Sidebar.jsx
+++ Admin-Dashboard/src/component/Sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import logo from "../../images/logo.png";
 import { SidebarData } from "../../Data/Data";
 
 const Sidebar = () => {
+  const [selected, setSelected] = useState(0)
   return (
+    
     <div className="Sidebar">
       <div className="logo">
         <img src={logo} alt="" />
@@ -16,12 +18,18 @@ const Sidebar = () => {
       <div className="menu">
         {SidebarData.map((item, index) => {
           return (
-            <div className="menuItem">
+            <div className={selected===index?'menuItem active': 'menuItem'} 
+            key={index}>
+              
               {item.icon}
               <span>{item.heading}</span>
             </div>
           );
         })}
+        <div className="menuItem">
+          <i class="ri-arrow-right-circle-line"></i>
+        </div>
+
       </div>
     </div>
   );
